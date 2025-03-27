d849423375a51f9e8f1289b38d0c039ef4f79c34
GisellaE <130516126+GisellaE@users.noreply.github.com>
2025-03-27 13:59:49 -0500

    Punto9.php

--- Punto9.php
+++ Punto9.php
@@ -1,5 +1,4 @@
 <?php
-// Definir una lista de frases
 $frases = [
     "El sol brilla en el cielo azul",
     "El cielo es azul y el mar también",
@@ -7,13 +6,12 @@ $frases = [
     "El azul del mar es hermoso"
 ];
 
-// Verificar si se enviaron las palabras desde el formulario
 $fraseModificada = "";
 if (isset($_GET['palabra_original']) && isset($_GET['palabra_nueva'])) {
     $palabraOriginal = $_GET['palabra_original'];
     $palabraNueva = $_GET['palabra_nueva'];
     
-    // Reemplazar la palabra en todas las frases
+
     $fraseModificada = array_map(function($frase) use ($palabraOriginal, $palabraNueva) {
         return str_ireplace($palabraOriginal, $palabraNueva, $frase);
     }, $frases);
